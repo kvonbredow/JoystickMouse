d15ea73eb3cd8ba98214e8f5f6d8cbbf30f6853a
kvonbredow <kylevonbredow@gmail.com>
2015-06-08 18:52:56 -0400

    small changes

--- src/joy2mouse.c
+++ src/joy2mouse.c
@@ -3,7 +3,33 @@
 #include "joystick.h"
 #include "xdo.h"
 
+void cursor_listener( struct js_event e );
+void click_listener( struct js_event e );
+
 int main( int argc, char **argv ) {
   printf( "%s\n", xdo_version() );
+  
+  //Initialize Joystick
+  init_listener(JOY_0);
+
+  //Register Listeners
+  int list_h[2];
+  list_h[0] = register_listener( cursor_listener, AXIS_ONLY );
+  list_h[1] = register_listener( click_listener, BUTTONS_ONLY );
+
+  while (1) sleep(60);
+
+  //Cleanup
+  deregister_listener( list_h[0] );
+  deregister_listener( list_h[1] );
+
   return 0;
+}
+
+void cursor_listener( struct js_event e ) {
+
+}
+
+void click_listener( struct js_event e ) {
+
 }
